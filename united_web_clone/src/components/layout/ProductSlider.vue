1f9b1d165db7a637533059a264c4572dca211b28
naso22 <naru.prog.0106@gmail.com>
2023-05-16 15:58:03 +0900

    sliderの枠組みを作成

---
+++ united_web_clone/src/components/layout/ProductSlider.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="block"></div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+div{
+    display: flex;
+    justify-content: center;
+    width: 100%;
+}
+
+.block{
+    max-width:1000px;
+    height: 50vh;
+    border:1.5px solid rgba(192, 192, 192, 0.88);
+}
+</style>
--- united_web_clone/src/pages/product/WoMenPage.vue
+++ united_web_clone/src/pages/product/WoMenPage.vue
@@ -1,17 +1,20 @@
 <template>
     <the-sarch></the-sarch>
     <category-filter></category-filter>
+    <product-slider></product-slider>
   <h2>WomenPage</h2>
 </template>
 
 <script>
 import TheSarch from "@/components/layout/TheSarch.vue";
 import CategoryFilter from '@/components/layout/CategoryFilter.vue'
+import ProductSlider from '@/components/layout/ProductSlider.vue'
 
 export default {
     components:{
         TheSarch,
-        CategoryFilter
+        CategoryFilter,
+        ProductSlider
     }
 }
 </script>
