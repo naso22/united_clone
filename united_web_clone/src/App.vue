67ab5558a83c8db2e3de1eca5da0e8b2805fe500
naso22 <naru.prog.0106@gmail.com>
2023-05-16 12:30:07 +0900

    headerの追加

--- united_web_clone/src/App.vue
+++ united_web_clone/src/App.vue
@@ -1,11 +1,29 @@
 <template>
+<the-header></the-header>
 <router-view></router-view>
 </template>
 
 <script>
-
+import TheHeader from "@/components/layout/TheHeader.vue";
 export default {
-
+components:{
+    TheHeader
+}
 }
 </script>
+
+<style>
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: "Roboto", sans-serif;
+}
+
+body {
+    margin: 0;
+}
+
+</style>
 
---
+++ united_web_clone/src/components/layout/TheHeader.vue
@@ -0,0 +1,68 @@
+<template>
+  <header>
+      <div class="header__content">
+          <div class="content">
+              <div>
+                  <a href="">
+                      <img class="header__img" src="https://store.united-arrows.co.jp/res/img/header/hamburger.svg" alt="">
+                  </a>
+              </div>
+              <div>
+                  <a href="">
+                      <img  src="https://store.united-arrows.co.jp/res/img/header/brands.svg" alt="">
+                  </a>
+              </div>
+          </div>
+          <div>
+              <h1>
+                  <a href="">
+                      <img src="https://store.united-arrows.co.jp/res/img/common/logo/brlogo-side-LTD000.svg" alt="">
+                  </a>
+              </h1>
+          </div>
+          <div class="content">
+              <div>
+                  <a href="">
+                      <img class="header__img" src="https://store.united-arrows.co.jp/res/img/common/ico-favourite.svg" alt="">
+                  </a>
+              </div>
+              <div>
+                  <a href="">
+                      <img src="https://img.icons8.com/?size=1x&id=15893&format=png" alt="">
+                  </a>
+              </div>
+          </div>
+      </div>
+  </header>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+header{
+    display: flex;
+    justify-content:center;
+}
+
+.header__content{
+    display: flex;
+    align-items: center;
+    width: 1024px;
+    justify-content: space-between;
+}
+
+.content{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.header__img{
+    margin-right: 25px;
+}
+
+h1{
+    margin: 0;
+}
+</style>
