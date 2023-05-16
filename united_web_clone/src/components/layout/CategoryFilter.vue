4edd19c78a78ddec1caa8f54dbff762fb91f81c1
naso22 <naru.prog.0106@gmail.com>
2023-05-16 15:35:43 +0900

    カテゴリーメニューの追加

---
+++ united_web_clone/src/components/layout/CategoryFilter.vue
@@ -0,0 +1,54 @@
+<template>
+  <ul>
+      <li>
+          <a href="">WOMEN</a>
+      </li>
+
+      <li>
+          <a href="">MEN</a>
+      </li>
+
+      <li>
+      <a href="">ALL</a>
+      </li>
+      <li>
+          <a href="">商品カテゴリー</a>
+      </li>
+      <li>
+          <a href="">ストアブランド</a>
+      </li>
+      <li>
+          <a href="">取扱ブランド</a>
+      </li>
+      <li>
+          <a href="">店舗検索</a>
+      </li>
+  </ul>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+ul{
+    display: flex;
+    justify-content: center;
+    background-color: #F6F5F4;
+    height:2.5rem;
+    align-items: center;
+}
+
+li{
+    margin-right: 50px;
+    list-style: none;
+    font-size: 0.75rem;
+    text-align: center;
+    letter-spacing: 0.1em;
+}
+
+a{
+    color: #858181;
+    text-decoration: inherit;
+}
+</style>
--- united_web_clone/src/pages/product/WoMenPage.vue
+++ united_web_clone/src/pages/product/WoMenPage.vue
@@ -1,14 +1,17 @@
 <template>
     <the-sarch></the-sarch>
+    <category-filter></category-filter>
   <h2>WomenPage</h2>
 </template>
 
 <script>
 import TheSarch from "@/components/layout/TheSarch.vue";
+import CategoryFilter from '@/components/layout/CategoryFilter.vue'
 
 export default {
     components:{
-        TheSarch
+        TheSarch,
+        CategoryFilter
     }
 }
 </script>
